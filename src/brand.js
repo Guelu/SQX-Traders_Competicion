854fde0d92f2f9e1584ff8a780914484ff3bab37
Miguel Jimenez <migue@users.noreply.github.com>
2026-06-16 11:17:27 +0200

    Update brand colors

--- src/brand.js
+++ src/brand.js
@@ -1,6 +1,6 @@
 export const LOGO_SVG = `
   <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M50 2 L93 26 V74 L50 98 L7 74 V26 Z" fill="#f97316"/>
+    <path d="M50 2 L93 26 V74 L50 98 L7 74 V26 Z" fill="#ffd100"/>
     <rect x="24" y="50" width="9" height="22" fill="#0a0a0a"/>
     <rect x="40" y="38" width="9" height="34" fill="#0a0a0a"/>
     <rect x="56" y="56" width="9" height="16" fill="#0a0a0a"/>
